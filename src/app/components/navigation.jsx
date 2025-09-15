29db5c34f73b5d173bf7e807b5ddc9e36a4a08d5
osuji chiaka <chiakajunior@yahoo.com>
2025-09-15 07:48:06 +0100

    working

--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -18,7 +18,7 @@ const NavigationBar = () => {
              <div className="flex flex-row justify-between text-[16px] text-[#354764] font-[600] items-center space-x-[40px]">
                 <Link href="/">Home</Link>
                  <Link href="/about">About</Link>
-                  <Link href="#">Events</Link>
+                  <Link href="/events">Events</Link>
                    <Link href="#">Resources</Link>
              </div>
 
