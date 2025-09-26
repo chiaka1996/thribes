1e0a20631fceb22acf682db9f53763a04b9d8548
osuji chiaka <chiakajunior@yahoo.com>
2025-09-26 06:44:58 +0100

    working

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import { useAllContext } from "../context/allcontext";
--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { useState, useRef, useEffect} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
@@ -7,8 +8,40 @@ import { usePathname } from "next/navigation";
 
 
 const NavigationBar = () => {
+    const closeDropdowns = useRef(null)
+    const closeMediaDropdown = useRef(null)
     const {toggleMobileNavbarFunction} = useAllContext();
      const pathname = usePathname();
+     const [openEvent, setOpenEvent] = useState(false);
+     const [openMedia, setOpenMedia] = useState(false)
+
+     // Close when clicking outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (closeDropdowns.current && !closeDropdowns.current.contains(event.target)) {
+        setOpenEvent(false);
+      }
+      if (closeMediaDropdown.current && !closeMediaDropdown.current.contains(event.target)) {
+        setOpenMedia(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
+     const toggleOpenEventLink = () => {
+        setOpenMedia(false)
+        setOpenEvent((prev) => !prev)
+     }
+
+     const toggleOpenMediaLink = () => {
+        setOpenEvent(false)
+        setOpenMedia((prev) => !prev)
+     }
 
 
     return(
@@ -25,9 +58,53 @@ const NavigationBar = () => {
 
              <div className="hidden md:flex justify-between space-x-[40px] text-[16px] text-[#354764]  items-center">
                <Link href="/"  className={`navLink ${pathname==="/" ? "text-[#087C72] font-[600]" : ""}`}>Home</Link>
+
                  <Link href="/about" className={`navLink ${pathname==="/about" ? "text-[#087C72] font-[600]" : ""}`}>About</Link>
-                  <Link href="/events" className={`navLink ${pathname==="/events" ? "text-[#087C72] font-[600]" : ""}`}>Events</Link>
-                   <Link href="/blog" className={`navLink ${pathname==="/blog" ? "text-[#087C72] font-[600]" : ""}`} >Resources</Link>
+
+                 <div className="navLink relative" ref={closeDropdowns}>
+                    <span onClick={toggleOpenEventLink} className={`flex justify-between items-center cursor-pointer ${pathname==="/events" ? "text-[#087C72] font-[600]" : ""}`}>
+                    <span>Events</span>
+                    <span className="material-symbols-outlined text-[24px] ">
+                        keyboard_arrow_down
+                    </span>
+                    </span>
+                    
+                    <div className={`${openEvent ? "block" : "hidden"} transition-all duration-500 ease-in-out min-w-[304px] text-secondaryColor text-[16px] z-50 bg-[#fff] space-y-[16px] absolute top-[100%] left-0 rounded-[20px] p-[16px] shadow-[0px_2px_10px_0px_#0000001A]`}>
+                        <div><Link href="/events" className={`cursor-pointer ${pathname==="/events" ? "text-[#087C72] font-[600]" : ""}`}>Tech Events</Link></div>
+                         
+                         <Link href="#" className="flex space-x-[10px]">
+                         <span>Football tournament</span>
+                         <button className="bg-[#FEF1E6] w-[93px] h-[28px] rounded-[10px] text-[#B15301] text-[12px]">Coming soon</button>
+                        </Link>
+                         <Link href="#" className="flex space-x-[10px]">
+                         <span>Outdoor fun</span>
+                         <button className="bg-[#FEF1E6] w-[93px] h-[28px] rounded-[10px] text-[#B15301] text-[12px]">Coming soon</button>
+                        </Link>
+                    </div>
+                 </div>
+                 
+                   
+                   <div className="navLink relative" ref={closeMediaDropdown}>
+                    <span onClick={toggleOpenMediaLink} className={`flex justify-between items-center cursor-pointer ${pathname==="/blog" ? "text-[#087C72] font-[600]" : ""}`}>
+                    <span>Media</span>
+                    <span className="material-symbols-outlined text-[24px] ">
+                        keyboard_arrow_down
+                    </span>
+                    </span>
+                    
+                    <div className={`${openMedia ? "block" : "hidden"} transition-all duration-500 ease-in-out min-w-[244px] text-secondaryColor text-[16px] z-50 bg-[#fff] space-y-[16px] absolute top-[100%] left-0 rounded-[20px] p-[16px] shadow-[0px_2px_10px_0px_#0000001A]`}>
+                        <div>
+                        <Link href="/events" className={`cursor-pointer ${pathname==="/blog" ? "text-[#087C72] font-[600]" : ""}`}>Blog & News</Link>
+                        </div>
+                         
+                         <Link href="#" className="flex space-x-[10px]">
+                         <span>Podcast</span>
+                         <button className="bg-[#FEF1E6] w-[93px] h-[28px] rounded-[10px] text-[#B15301] text-[12px]">Coming soon</button>
+                        </Link>
+                    </div>
+                 </div>
+
+                 <Link href="#" className={`navLink ${pathname==="/about" ? "text-[#087C72] font-[600]" : ""}`}>Shop</Link>
              </div>
 
                 <div className="flex w-[30px] md:w-[152px] justify-between items-center ">
